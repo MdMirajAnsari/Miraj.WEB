7e4f27ec69356d8bf89def304eaa31aa4668e43f
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-04-18 08:36:32 +0530

    Refactor ServiceCard component to include PropTypes validation and clean up unused options

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,6 +1,6 @@
-import React from "react";
 import { motion } from "framer-motion";
 import { styles } from "../styles";
+import PropTypes from "prop-types";
 import { services } from "../constants";
 import { fadeIn, textVariant } from "../utils/motion";
 import { SectionWrapper } from "../hoc";
@@ -12,11 +12,7 @@ const ServiceCard = ({ index, title, icon }) => {
       className="xs:w-[250px] w-full card-gradient p-[1px] rounded-[20px] shadow-card"
     >
       <div
-        options={{
-          max: 45,
-          scale: 1,
-          speed: 450,
-        }}
+      
         className="bg-jetLight rounded-[20px] py-5 px-12 min-h-[280px] flex justify-evenly items-center flex-col"
       >
         <img src={icon} alt={title} className="w-16 h-16 object-contain" />
@@ -27,6 +23,12 @@ const ServiceCard = ({ index, title, icon }) => {
     </motion.div>
   );
 };
+ServiceCard.propTypes = {
+  index: PropTypes.number.isRequired,
+  title: PropTypes.string.isRequired,
+  icon: PropTypes.string.isRequired,
+};
+
 
 const About = () => {
   return (
