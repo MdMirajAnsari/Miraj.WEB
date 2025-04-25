d070f1a79ad81ed762f18c87f91e56e0e61fd098
Clever Cottonmouth <mirajhad@users.noreply.github.com>
2025-04-26 00:14:16 +0530

    Refactor Hero component for improved readability and consistency in JSX formatting

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,12 +1,11 @@
-import { motion } from 'framer-motion';
-import { styles } from '../styles';
-import {  bwmap, worldmap } from '../assets';
-import { NetworkWebBackground } from './canvas';
+import { motion } from "framer-motion";
+import { styles } from "../styles";
+import { bwmap, worldmap } from "../assets";
+import { NetworkWebBackground } from "./canvas";
 
 const Hero = () => {
   return (
     <>
-    
       {/* <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
         <img
           src={bwmap}
@@ -14,62 +13,67 @@ const Hero = () => {
           className="w-full h-full sm:block hidden object-cover"
         />
       </div> */}
-     
+
       <div className="absolute top-0 left-0 z-0 h-[100vh] w-screen">
-      
         <img
           src={worldmap}
           alt="world map"
           className="w-full h-full sm:hidden block object-cover"
         />
       </div>
-      
+
       <section
         className="relative flex sm:flex-row flex-col w-full h-screen mx-auto 
-        sm:bg-hero bg-hero-mobile overflow-hidden">
-          <NetworkWebBackground />
+        sm:bg-hero bg-hero-mobile overflow-hidden"
+      >
+        <NetworkWebBackground />
         <div
           className={`absolute inset-0 sm:top-[250px] top-[150px] 
           lg:top-[150px] xl:top-[250px] ${styles.paddingX} 
           max-w-7xl mx-auto flex flex-row items-start
-          justify-between gap-3`}>
+          justify-between gap-3`}
+        >
           <div className="flex flex-col justify-center items-center mt-5 ml-3">
             <div className="w-5 h-5 rounded-full bg-[#0a0a0a] sm:hidden" />
             <div className="w-1 sm:h-80 h-40 bw-gradient sm:hidden" />
           </div>
-          
+
           <div>
-          
             <h1
-              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}>
+              className={`${styles.heroHeadText} text-eerieBlack font-poppins uppercase`}
+            >
               HI, I&apos;m &nbsp;
               <span
                 className="sm:text-battleGray sm:text-[90px] 
                 text-eerieBlack text-[50px] font-mova
-                font-extrabold uppercase">
+                font-extrabold uppercase"
+              >
                 MOHAMMAD MIRAJ
               </span>
             </h1>
             <p className={`${styles.heroSubText} mt-2 text-eerieBlack`}>
-               Software Engineer. <br className="sm:block hidden" />
+              Software Engineer. <br className="sm:block hidden" />
               Bangalore, India.
             </p>
           </div>
           <div
             className="w-screen flex flex-col items-start 
-            justify-center sm:-ml-[3rem] xxs:mt-4"></div>
+            justify-center sm:-ml-[3rem] xxs:mt-4"
+          ></div>
 
           <div></div>
         </div>
 
         <div
           className="absolute xs:bottom-10 bottom-32 w-full 
-          flex justify-center items-center">
+          flex justify-center items-center"
+        >
           <a href="#about">
             <div
               className="w-[35px] h-[64px] rounded-3xl border-4 
              border-dim flex
-            justify-center items-start p-2">
+            justify-center items-start p-2"
+            >
               <motion.div
                 animate={{
                   y: [0, 24, 0],
@@ -77,7 +81,7 @@ const Hero = () => {
                 transition={{
                   duration: 1.5,
                   repeat: Infinity,
-                  repeatType: 'loop',
+                  repeatType: "loop",
                 }}
                 className="w-3 h-3 rounded-full bg-taupe mb-1"
               />
@@ -86,15 +90,15 @@ const Hero = () => {
         </div>
 
         {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
-        {/* <div>
+        {/* Your image comes here. Feel free to remove image if you don't plan to have one.*/}
+        <div>
           <img
-            className="absolute bottom-0 ml-[50vw] 
-            lg:ml-[75vw] md:ml-[60vw] xmd:ml-[60vw] 2xl:ml-[83vw]
-            sm:h-[90vh] md:h-[70vh] xl:h-[80vh]"
-            src={shaq}
-            alt="shaquille"
+            className="absolute top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 
+      sm:h-[90vh] md:h-[70vh] xl:h-[80vh] opacity-10 sm:opacity-10"
+            src={worldmap}
+            alt="worldmap"
           />
-        </div> */}
+        </div>
       </section>
     </>
   );
