3f0e351b04606dc9857c850aa7f101602abfa5f8
mirajhad <mirajhadish48@gmail.com>
2026-05-02 11:32:14 +0530

    feat: add new YouTube videos and update categories in YouTube component

--- src/components/YouTube.jsx
+++ src/components/YouTube.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import { useState } from 'react';
 import { styles } from '../styles';
 
 const YouTube = () => {
@@ -30,11 +30,172 @@ const YouTube = () => {
       title: 'Video 4',
       url: 'https://www.youtube.com/watch?v=X48VuDVv0do',
       category: 'coding'
+    },
+    {
+      id: 5,
+      embedId: 'Rgx8dpiPwpA',
+      title: 'Video 5',
+      url: 'https://www.youtube.com/watch?v=Rgx8dpiPwpA',
+      category: 'fun'
+    },
+    {
+      id: 6,
+      embedId: '5bId3N7QZec',
+      title: 'Video 6',
+      url: 'https://www.youtube.com/watch?v=5bId3N7QZec',
+      category: 'fun'
+    },
+    {
+      id: 7,
+      embedId: 'ebniIyejb1E',
+      title: 'Video 7',
+      url: 'https://www.youtube.com/watch?v=ebniIyejb1E&t=18s',
+      category: 'angular interview'
+    },
+    {
+      id: 8,
+      embedId: 'uUnx3M1_uAg',
+      title: 'Video 8',
+      url: 'https://www.youtube.com/watch?v=uUnx3M1_uAg',
+      category: 'angular interview'
+    },
+    {
+      id: 9,
+      embedId: '_gfmaVcMFLk',
+      title: 'Video 9',
+      url: 'https://www.youtube.com/watch?v=_gfmaVcMFLk',
+      category: 'angular interview'
+    },
+    {
+      id: 10,
+      embedId: 'w2Mvky1rio8',
+      title: 'Video 10',
+      url: 'https://www.youtube.com/watch?v=w2Mvky1rio8',
+      category: 'angular interview'
+    },
+    {
+      id: 11,
+      embedId: 'SYHCP23PRGk',
+      title: 'Video 11',
+      url: 'https://www.youtube.com/watch?v=SYHCP23PRGk',
+      category: 'angular interview'
+    },
+    {
+      id: 12,
+      embedId: 'JR548Nt7r7I',
+      title: 'Video 12',
+      url: 'https://www.youtube.com/watch?v=JR548Nt7r7I',
+      category: 'angular interview'
+    },
+    {
+      id: 13,
+      embedId: 'GuIsHRpp4y4',
+      title: 'Video 13',
+      url: 'https://www.youtube.com/watch?v=GuIsHRpp4y4',
+      category: 'angular interview'
+    },
+    {
+      id: 14,
+      embedId: '8ToYU5SS0z8',
+      title: 'Video 14',
+      url: 'https://www.youtube.com/watch?v=8ToYU5SS0z8',
+      category: 'angular interview'
+    },
+    {
+      id: 15,
+      embedId: 'qHRvtww2V3U',
+      title: 'Video 15',
+      url: 'https://www.youtube.com/watch?v=qHRvtww2V3U',
+      category: 'angular interview'
+    },
+    {
+      id: 16,
+      embedId: 'kVA17khNYvw',
+      title: 'Video 16',
+      url: 'https://www.youtube.com/watch?v=kVA17khNYvw',
+      category: 'angular interview'
+    },
+    {
+      id: 17,
+      embedId: 'i5gRxiQFupU',
+      title: 'Video 17',
+      url: 'https://www.youtube.com/watch?v=i5gRxiQFupU',
+      category: 'angular interview'
+    },
+    {
+      id: 18,
+      embedId: 'fcZCNSaNTFo',
+      title: 'Video 18',
+      url: 'https://www.youtube.com/watch?v=fcZCNSaNTFo',
+      category: 'angular interview'
+    },
+    {
+      id: 19,
+      embedId: 'NBe9qgUKGyA',
+      title: 'Video 19',
+      url: 'https://www.youtube.com/watch?v=NBe9qgUKGyA',
+      category: 'angular interview'
+    },
+    {
+      id: 20,
+      embedId: 'uo-ysRYnlqU',
+      title: 'Video 20',
+      url: 'https://www.youtube.com/watch?v=uo-ysRYnlqU',
+      category: 'angular interview'
+    },
+    {
+      id: 21,
+      embedId: '9hWvhAiDKqQ',
+      title: 'Video 21',
+      url: 'https://www.youtube.com/watch?v=9hWvhAiDKqQ',
+      category: 'angular interview'
+    },
+    {
+      id: 22,
+      embedId: 'Z0A7FjGXna8',
+      title: 'Video 22',
+      url: 'https://www.youtube.com/watch?v=Z0A7FjGXna8',
+      category: 'angular interview'
+    },
+    {
+      id: 23,
+      embedId: '9mE02hYx4Sk',
+      title: 'Video 23',
+      url: 'https://www.youtube.com/watch?v=9mE02hYx4Sk',
+      category: 'angular interview'
+    },
+    {
+      id: 24,
+      embedId: '-0exw-9YJBo',
+      title: 'Video 24',
+      url: 'https://www.youtube.com/watch?v=-0exw-9YJBo&list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm',
+      category: 'mern project'
+    },
+    {
+      id: 25,
+      embedId: 'enopDSs3DRw',
+      title: 'Video 25',
+      url: 'https://www.youtube.com/watch?v=enopDSs3DRw&list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm&index=2',
+      category: 'mern project'
+    },
+    {
+      id: 26,
+      embedId: 'mvfsC66xqj0',
+      title: 'Video 26',
+      url: 'https://www.youtube.com/watch?v=mvfsC66xqj0&list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm&index=3',
+      category: 'mern project'
+    },
+    {
+      id: 27,
+      embedId: 'UXjMo25Nnvc',
+      title: 'Video 27',
+      url: 'https://www.youtube.com/watch?v=UXjMo25Nnvc&list=PLillGF-RfqbbQeVSccR9PGKHzPJSWqcsm&index=4&pbjreload=102',
+      category: 'mern project'
     }
   ]);
 
   const [selectedCategory, setSelectedCategory] = useState('all');
-  const categories = ['all', 'tech', 'interview', 'fun', 'coding', 'projects'];
+  const categories = ['all', 'tech', 'interview', 'angular interview', 'mern project', 'fun', 'coding', 'projects'];
 
   const filteredVideos = selectedCategory === 'all' 
     ? videos 
